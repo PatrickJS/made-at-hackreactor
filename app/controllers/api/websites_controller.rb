605b5e104290fb8ac8f270ff1666d8c57aceaebf
gdi2290 <github@gdi2290.com>
2013-06-18 14:49:57 -0700

    create api namespace, init schema

---
+++ app/controllers/api/websites_controller.rb
@@ -0,0 +1,60 @@
+module Api
+  class WebsitesController < ApplicationController
+    respond_to :json
+
+    # GET /api/websites.json
+    def index
+      @websites = Website.all
+      render json: @websites
+    end
+
+    # GET /api/websites/1.json
+    def show
+      @website = Website.find(params[:id])
+
+      render json: @website
+    end
+
+    # GET /api/websites/new.json
+    def new
+      @website = Website.new
+      render json: @website
+    end
+
+    # GET /api/websites/1/edit
+    def edit
+      @website = Website.find(params[:id])
+    end
+
+    # POST /api/websites.json
+    def create
+      @website = Website.new(params[:website])
+
+      if @website.save
+        render json: @website, status: :created, location: @website
+      else
+        render json: @website.errors, status: :unprocessable_entity
+      end
+    end
+
+    # PUT /api/websites/1.json
+    def update
+      @website = Website.find(params[:id])
+
+      if @website.update_attributes(params[:website])
+        head :no_content
+      else
+        render json: @website.errors, status: :unprocessable_entity
+      end
+    end
+
+    # DELETE /api/websites/1.json
+    def destroy
+      @website = Website.find(params[:id])
+      @website.destroy
+
+      head :no_content
+    end
+
+  end
+end
