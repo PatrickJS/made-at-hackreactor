33d2fdbb5caff8da1afbad68f3ba00b973cf6ffd
gdi2290 <github@gdi2290.com>
2013-06-21 04:27:05 -0700

    update readme with code snippet, add janky iframe share buttons, add html2canvas for screenshots

--- README.md
+++ README.md
@@ -1,14 +1,42 @@
 # Made at Hack Reactor
 
-bundle install  
-gem install foreman  
-rake db:migrate  
-foreman server -f Procfile.dev  
-localhost:5100  
+bundle install
+gem install foreman
+rake db:migrate
+foreman server -f Procfile.dev
+localhost:5100
 
-website RESTful API  
-/api/websites/:id  
-$ curl -H 'Accept:application/vnd.hackreactor.v1' http://localhost:3000/api/websites  
+website RESTful API
+/api/websites/:id
+$ curl -H 'Accept:application/vnd.hackreactor.v1' http://localhost:3000/api/websites
+
+Work in Progress scripttag.js
+<pre>
+(function() {
+  var HackReactor = {};
+  HackReactor['func'] = function(data) {
+    window['yolo'] = data;
+    this.html = document.createElement(data.create);
+    this.html.src = data.html;
+    document.getElementsByTagName('body')[0].appendChild(this.html);
+  };
+  HackReactor['jsonp'] = function(url, cb) {
+    this.yolo = document.createElement('script');
+    this.rand = 'yolo' + ~~(Math.random()*8888);
+    this.yolo.src = url+'?callback='+this.rand;
+    this.yolo.setAttribute('async', 'true');
+    (document.getElementsByTagName('head') || document.getElementsByTagName('body'))[0].appendChild(this.yolo);
+    window[this.rand] = cb;
+  };
+  HackReactor.jsonp('http://hackreactor.herokuapp.com/callback', HackReactor.func);
+  console.log('Hello with love from Hack Reactor');
+}());
+</pre>
+
+
+
+
+
 
 Copyright 2013, [Hack Reactor, LLC](http://hackreactor.com). All rights reserved.
 
--- app/assets/javascripts/views/website.js
+++ app/assets/javascripts/views/website.js
@@ -21,9 +21,13 @@ HackReactor.Views.Website = Backbone.View.extend({
                                           '<a href="<%= url %>" target="_blank"><span class="color-text"><%= url %></span></a>'+
                                           '<p><%= content %></p>'+
                                           '<div class="like-tweet">'+
-                                            '<a class="button" href="#Like"><i class="icon-thumbs-up"></i></a>'+
-                                            '<a class="button" href="#Tweet"><i class="icon-twitter"></i></a>'+
-                                            '<a class="button" href="#Tweet"><i class="icon-github-2"></i></a>'+
+'<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://hackreactor.herokuapp.com/" data-via="yolo" data-related="yolo" data-hashtags="tag" data-dnt="true">Tweet</a>'+
+"<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script><br>"+
+'<iframe src="//www.facebook.com/plugins/like.php?href=http%3A%2F%2Fhackreactor.herokuapp.com%2F&amp;send=false&amp;layout=button_count&amp;width=450&amp;show_faces=false&amp;font&amp;colorscheme=dark&amp;action=like&amp;height=21&amp;appId=409068965874161" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:21px;" allowTransparency="true"></iframe><br>'+
+'<iframe src="http://ghbtns.com/github-btn.html?user=gdi2290&repo=slides&type=watch&count=true"allowtransparency="true" frameborder="0" scrolling="0" width="110" height="20"></iframe>'+
+                                            //'<a class="button" href="#Like"><i class="icon-thumbs-up"></i></a>'+
+                                            //'<a class="button" href="#Tweet"><i class="icon-twitter"></i></a>'+
+                                            //'<a class="button" href="#Tweet"><i class="icon-github-2"></i></a>'+
                                           '</div>'+
                                         '</div>'+
                                       '</div>'+
