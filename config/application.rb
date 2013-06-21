fec87e82fe68130a0b5ec618f26c362d08b30067
gdi2290 <github@gdi2290.com>
2013-06-20 23:45:47 -0700

    wip

--- config/application.rb
+++ config/application.rb
@@ -59,5 +59,6 @@ module MadeAtHackreactor
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.assets.paths << Rails.root.join("#{Rails.root}/app/assets/fonts")
+    config.assets.initialize_on_precompile = false
   end
 end
