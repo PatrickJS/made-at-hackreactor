dd0cc60909fbdec6f0c16b561b813bc980faf33e
gdi2290 <github@gdi2290.com>
2013-06-20 23:42:09 -0700

    setting up redis

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -31,10 +31,10 @@ after_fork do |server, worker|
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
   end
   # If you are using Redis but not Resque, change this
-  # if defined?(Resque)
-  #   ENV["OPENREDIS_URL"] ||= "redis://127.0.0.1:6379"
-  #   uri = URI.parse(ENV["OPENREDIS_URL"])
-  #   Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-  #   Rails.logger.info('Connected to Redis')
-  # end
+  if defined?(Resque)
+    ENV["OPENREDIS_URL"] ||= "redis://127.0.0.1:6379"
+    uri = URI.parse(ENV["OPENREDIS_URL"])
+    Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+    Rails.logger.info('Connected to Redis')
+  end
 end
